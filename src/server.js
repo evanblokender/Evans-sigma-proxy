c8c878a615ddf7c0ada3c8ea7cae8cee57443d1f
evanblokender <157309371+evanblokender@users.noreply.github.com>
2026-03-05 23:06:21 +0000

    Add files via upload

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 const http = require("http");
 const express = require("express");
 const path = require("path");
+const { server: wisp } = require("@mercuryworkshop/wisp-js/server");
 
 let scramjetPath, baremuxPath, epoxyPath, libcurlPath;
 try {
-  // new CI build exports scramjetPath
   const sj = require("@mercuryworkshop/scramjet/path");
   scramjetPath = sj.scramjetPath || sj.publicPath;
   baremuxPath  = require("@mercuryworkshop/bare-mux/node").baremuxPath;
@@ -15,7 +15,6 @@ try {
   process.exit(1);
 }
 
-const { createServer: createWispServer } = require("wisp-server-node");
 const PORT = process.env.PORT || 8080;
 const app = express();
 
@@ -27,9 +26,13 @@ app.use(express.static(path.join(__dirname, "../public")));
 app.get("*", (_, res) => res.sendFile(path.join(__dirname, "../public/index.html")));
 
 const server = http.createServer(app);
+
 server.on("upgrade", (req, socket, head) => {
-  if (req.url.startsWith("/wisp/")) createWispServer(req, socket, head);
-  else socket.destroy();
+  if (req.url.startsWith("/wisp/")) {
+    wisp.routeRequest(req, socket, head);
+  } else {
+    socket.destroy();
+  }
 });
 
 server.listen(PORT, () => console.log(`🔥 Evan's Sigma Proxy on port ${PORT}`));
